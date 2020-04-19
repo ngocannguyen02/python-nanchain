2b18c990fb3ebea4f46a11b53b6cdd844840c7e7
Ngoc an Nguyen <ngocannguyen@hokuto.local>
2020-04-19 17:32:02 -0400

    add network feature, validating all the block in chain, concensus algo to update node with authoritave chain

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
+import requests
+
 from time import time
+from urllib.parse import urlparse
 
 class Blockchain(object):
 
@@ -9,6 +12,12 @@ class Blockchain(object):
         self.current_transactions = []
         # initial block
         self.new_block(previous_hash=1, proof=100)
+        self.nodes = set() # make sure no duplicate will be inserted
+
+    def register_node(self, address):
+        # address for example http://192.168.0.5:5000
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     def new_block(self, proof, previous_hash=None):
         block = {
@@ -56,3 +65,50 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    def valid_chain(self, chain):
+        # valid all the blocks in the chain
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        # Make sure our current node has the longest chain over all the network
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
